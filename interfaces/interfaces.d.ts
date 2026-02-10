37d264a1ab1e013835a6eed75a8f6a54d32ba282
sazid-zero <sharif.sazid.3@gmail.com>
2026-02-10 15:06:02 +0600

    feat: commit interfaces/

---
+++ interfaces/interfaces.d.ts
@@ -0,0 +1,270 @@
+interface Movie {
+  id: number;
+  title: string;
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+interface TrendingMovie {
+  searchTerm: string;
+  movie_id: number;
+  title: string;
+  count: number;
+  poster_url: string;
+}
+
+interface MovieDetails {
+  adult: boolean;
+  backdrop_path: string | null;
+  belongs_to_collection: {
+    id: number;
+    name: string;
+    poster_path: string;
+    backdrop_path: string;
+  } | null;
+  budget: number;
+  genres: {
+    id: number;
+    name: string;
+  }[];
+  homepage: string | null;
+  id: number;
+  imdb_id: string | null;
+  original_language: string;
+  original_title: string;
+  overview: string | null;
+  popularity: number;
+  poster_path: string | null;
+  production_companies: {
+    id: number;
+    logo_path: string | null;
+    name: string;
+    origin_country: string;
+  }[];
+  production_countries: {
+    iso_3166_1: string;
+    name: string;
+  }[];
+  release_date: string;
+  revenue: number;
+  runtime: number | null;
+  spoken_languages: {
+    english_name: string;
+    iso_639_1: string;
+    name: string;
+  }[];
+  status: string;
+  tagline: string | null;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+interface TrendingCardProps {
+  movie: TrendingMovie;
+  index: number;
+}
+
+// TV Show Interfaces
+interface TVShow {
+  id: number;
+  name: string;
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  original_language: string;
+  original_name: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  first_air_date: string;
+  vote_average: number;
+  vote_count: number;
+  origin_country: string[];
+}
+
+interface TVShowDetails {
+  adult: boolean;
+  backdrop_path: string | null;
+  created_by: {
+    id: number;
+    name: string;
+    profile_path: string | null;
+  }[];
+  episode_run_time: number[];
+  first_air_date: string;
+  genres: {
+    id: number;
+    name: string;
+  }[];
+  homepage: string | null;
+  id: number;
+  in_production: boolean;
+  languages: string[];
+  last_air_date: string;
+  name: string;
+  networks: {
+    id: number;
+    name: string;
+    logo_path: string | null;
+    origin_country: string;
+  }[];
+  number_of_episodes: number;
+  number_of_seasons: number;
+  origin_country: string[];
+  original_language: string;
+  original_name: string;
+  overview: string | null;
+  popularity: number;
+  poster_path: string | null;
+  seasons: {
+    air_date: string;
+    episode_count: number;
+    id: number;
+    name: string;
+    overview: string;
+    poster_path: string | null;
+    season_number: number;
+  }[];
+  status: string;
+  tagline: string | null;
+  type: string;
+  vote_average: number;
+  vote_count: number;
+}
+
+// Person Interfaces
+interface Person {
+  id: number;
+  name: string;
+  profile_path: string | null;
+  known_for_department: string;
+  popularity: number;
+  known_for?: (Movie | TVShow)[];
+}
+
+interface PersonDetails {
+  adult: boolean;
+  also_known_as: string[];
+  biography: string;
+  birthday: string | null;
+  deathday: string | null;
+  gender: number;
+  homepage: string | null;
+  id: number;
+  imdb_id: string | null;
+  known_for_department: string;
+  name: string;
+  place_of_birth: string | null;
+  popularity: number;
+  profile_path: string | null;
+}
+
+interface Credits {
+  cast: {
+    adult: boolean;
+    gender: number;
+    id: number;
+    known_for_department: string;
+    name: string;
+    original_name: string;
+    popularity: number;
+    profile_path: string | null;
+    character: string;
+    credit_id: string;
+    order: number;
+  }[];
+  crew: {
+    adult: boolean;
+    gender: number;
+    id: number;
+    known_for_department: string;
+    name: string;
+    original_name: string;
+    popularity: number;
+    profile_path: string | null;
+    credit_id: string;
+    department: string;
+    job: string;
+  }[];
+}
+
+// Video Interface
+interface Video {
+  id: string;
+  iso_639_1: string;
+  iso_3166_1: string;
+  key: string;
+  name: string;
+  site: string;
+  size: number;
+  type: string;
+  official: boolean;
+  published_at: string;
+}
+
+// Review Interface
+interface Review {
+  author: string;
+  author_details: {
+    name: string;
+    username: string;
+    avatar_path: string | null;
+    rating: number | null;
+  };
+  content: string;
+  created_at: string;
+  id: string;
+  updated_at: string;
+  url: string;
+}
+
+// Genre Interface
+interface Genre {
+  id: number;
+  name: string;
+}
+
+// Trending Interface
+interface TrendingItem {
+  id: number;
+  media_type: "movie" | "tv" | "person";
+  adult?: boolean;
+  backdrop_path?: string;
+  genre_ids?: number[];
+  original_language?: string;
+  overview?: string;
+  popularity: number;
+  poster_path?: string;
+  vote_average?: number;
+  vote_count?: number;
+  // Movie specific
+  title?: string;
+  original_title?: string;
+  release_date?: string;
+  video?: boolean;
+  // TV specific
+  name?: string;
+  original_name?: string;
+  first_air_date?: string;
+  origin_country?: string[];
+  // Person specific
+  profile_path?: string | null;
+  known_for_department?: string;
+  known_for?: (Movie | TVShow)[];
+}
+
+// Media Types
+type MediaType = "movie" | "tv" | "person";
+type TimeWindow = "day" | "week";
